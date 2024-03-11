2b02e09a2d184a047527934b892ab3724e1fd6f6
Avitoshsandve <129187663+Avitoshsandve@users.noreply.github.com>
2024-03-11 21:32:05 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,12 @@
+let sum=(a,b)=>a+b;
+console.log(sum(5,6))
+
+
+let subtract=(c,d)=>c-d;
+console.log(subtract(1,3))
+
+let multiply=(e,f)=>e*f;
+console.log(multiply(7,8))
+
+let divide=(g,h)=>g/h;
+console.log(divide(1,5))
