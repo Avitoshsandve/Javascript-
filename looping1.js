c1cb6e85bc8e58a60f7d8926c991f32cc4acdb3e
Avitoshsandve <129187663+Avitoshsandve@users.noreply.github.com>
2024-03-11 21:06:24 +0530

    Add files via upload

---
+++ looping1.js
@@ -0,0 +1,21 @@
+//for
+for(let i=0;i<=10;i++)
+{
+    console.log(i)
+}
+
+//while
+let j=6;
+while(j < 10)
+{
+    console.log(j);
+    j--;
+}
+
+//do while
+let k=0;
+do{
+    console.log(k)
+    k++;
+}while(k<=5)
+
