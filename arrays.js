55a6adb6df2cc046d5c587a739378cbf31c41827
Avitoshsandve <129187663+Avitoshsandve@users.noreply.github.com>
2024-03-12 17:23:55 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,11 @@
+let arr=[6,5,4,3,2,1];
+console.log(arr);
+
+//we can also get the value based on index
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr.length);
